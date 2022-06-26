56adf436fc7e78367c96c08b8792e334a6bb7642
Catif <bradley.barbier@outlook.fr>
2022-06-26 22:07:29 +0200

    Updating Commentary

--- step1.ps1
+++ step1.ps1
@@ -1,7 +1,7 @@
 ﻿# Installation de Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# permet d'accepter l'installation de tous les programmes
+# permet d'accepter l'installation de tous les programmes ci-dessous
 choco feature enable -n=allowGlobalConfirmation
 
 # Listes des logiciels à télécharger (pendre les noms sur le site Chocolatey => https://community.chocolatey.org)
