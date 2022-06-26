bcc240ec44be6d01236783bfe9ae4216ea5c5acd
Catif <bradley.barbier@outlook.fr>
2022-06-26 21:59:22 +0200

    Update step1.ps1

--- step1.ps1
+++ step1.ps1
@@ -1,46 +1,59 @@
 ﻿# Installation de Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# "-y" permet d'accepter l'installation
-# Logiciel général
-choco install unchecky -y
-choco install vivaldi -y
-choco install discord -y
-choco install winrar -y
-choco install vlc -y
-choco install obs-studio -y
-choco install paint.net -y
-choco install everything -y
-choco install sharex -y
-choco install 1clipboard -y
-choco install eartrumpet -y
-choco install razer-synapse-3 -y
-choco install icue -y
-choco install qbittorrent -y
-choco install audacity -y
-choco install telegram -y
+# permet d'accepter l'installation de tous les programmes
+choco feature enable -n=allowGlobalConfirmation
+
+# Listes des logiciels à télécharger (pendre les noms sur le site Chocolatey => https://community.chocolatey.org)
+$logiciels = @(
+    # Logiciel général
+    'unchecky',
+    'vivaldi',
+    'discord',
+    'winrar',
+    'vlc',
+    'obs-studio',
+    'paint.net',
+    'everything',
+    'sharex',
+    '1clipboard',
+    'eartrumpet',
+    'razer-synapse-3',
+    'icue',
+    'qbittorrent',
+    'audacity',
+    'telegram',
 
 
-# Logiciel de jeu
-choco install hamachi -y
-choco install origin -y
-choco install steam-client -y
-choco install epicgameslauncher -y
 
+    # Logiciel de jeu
+    'hamachi',
+    'origin',
+    'steam-client',
+    'epicgameslauncher',
 
-# Variable d'environnement
-choco install git -y
-choco install nodejs-lts -y
-choco install php -y
-choco install composer -y
-choco install actiona -y
-choco install python -y
+    
+    
+    # Variable d'environnement
+    'git',
+    'nodejs-lts',
+    'php',
+    'composer',
+    'python',
+    
+    
+    
+    # Logiciel Développeur
+    'wamp-server',
+    'actiona',
+    'vscode',
+    'visualstudio-installer',
+    'filezilla',
+    'github-desktop',
+    'putty'
+)
 
-
-# Logiciel Développeur
-choco install wamp-server -y
-choco install vscode -y
-choco install visualstudio-installer -y
-choco install filezilla -y
-choco install github-desktop -y
-choco install putty -y
+# Boucle d'installation de tous les logiciels
+Foreach($logiciel in $logiciels) {
+    choco install $logiciel
+}
